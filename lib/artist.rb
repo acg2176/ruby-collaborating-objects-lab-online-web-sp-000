d26436937c5a6120d8b4bb530128f795e8a7b26e
Anna Guerrero <acg2176@columbia.edu>
2020-04-24 04:20:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def all
+    @@all
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def self.find_or_create_by_name(name)
+    artist = self.all.find {|artist| artist.name == name} #instance found in the @@all where name is found
+    if artist
+      artist #returns the instance
+    else
+      new = self.new(name) #creates new instance Artist, returns the instance
+      @@all << new
+      new
+    end
+  end
+
+  def print_songs
+    songs.each do |song|
+      puts "#{song.name}"
+    end
+  end
+end
